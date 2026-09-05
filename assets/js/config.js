2970d8069a712ccb4233fb5ebe68ed05c1f9464d
jinkyborjaa-web <jinkyborjaa@gmail.com>
2026-09-05 10:03:12 +0800

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,4 +0,0 @@
-const productionApiUrl = 'https://e-index-ir8v.onrender.com/api';
-const isLocalDevelopment = ['localhost', '127.0.0.1', ''].includes(window.location.hostname);
-
-window.__API_URL__ = window.__API_URL__ || (isLocalDevelopment ? '' : productionApiUrl);
